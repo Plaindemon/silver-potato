82ba0f6f90033f9ffdfd6218e484419f7b4861ab
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-04 16:47:22 -0700

    testing writefile

--- index.js
+++ index.js
@@ -9,73 +9,96 @@ var inquirer = require('inquirer');
 
 // console.log(inquirer);
 
-inquirer
-    .prompt([
+const questions = [
 /* Pass your questions in here */
   {
     type: 'input',
-    name: 'Title',
+    name: 'title',
     message: 'What is the title of your project?',
 
   },
   {
     type: 'input',
-    name: 'Description',
+    name: 'description',
     message: 'What is the description?',
 
   },
   {
     type: 'input',
-    name: 'Table Of Contents',
+    name: 'confirm',
     message: 'Add a table of contents?',
 
   },
   {
     type: 'checkbox',
-    name: 'License',
-    message: 'What are you going to use?',
-    choices: ['option 1', 'option 2', 'opt 3', 'opt 4', 'opt 5']
+    message: 'What languages did you use??',
+    name: 'language',
+    choices: ['HTML', 'CSS', 'JavaScript', 'SQL']
   },
   {
     type: 'checkbox',
-    name: 'Contact',
+    name: 'license',
+    message: 'What license is required to use?',
+    choices: ['MIT', 'ISC', 'opt 3', 'opt 4', 'opt 5']
+  },
+  {
+    type: 'checkbox',
+    name: 'contact',
     message: 'What is your preferred method of contact?',
     choices: ['Email', 'Phone', 'Work Phone']
   },
   {
     type: 'input',
-    name: 'Email',
+    name: 'email',
     message: 'What is your email address?'
 
   },
   {
     type: 'input',
-    name: 'Phone',
+    name: 'phone',
     message: 'What is your number Number (or Work Number)?'
 
   },
   {
     type: 'input',
-    name: 'Github',
+    name: 'github',
     message: 'What is your github username?',
 
   },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'What are the instillation instructs needed to run the project?'
+  },
+  { type: 'input',
+    name: 'contribution',
+    message: 'Who contributed or worked to the project?'
 
+  },
+  {
+    type: 'input',
+    name: 'contact',
+    message: 'What are the best ways to contact you for inquiries or questions?'
+  }
+];
 
-
-    ])
+inquirer
+  .prompt(questions)
     .then((answers) => {
         // Use user feedback for... whatever!!
+        // CREATE readme file
         console.log(answers);
-        fs.writeFile("input.txt", JSON.stringify(answers, null, '\t'), function(err) {
+        generateMarkdown(answers);
+        fs.writeFile("./README.md", JSON.stringify(generateMarkdown(answers), null, '\t'), (err) =>{
     
-          if (err) {
-            return console.log(err);
-          }
-      
+          if (err) throw err;
+          console.log('Saved!');
+        
           console.log("Success!");
-      
+        
         });
+        
+        
       })
       .catch((error) => {
         if (error.isTtyError) {
@@ -85,13 +108,11 @@ inquirer
         }
       });
 
-      // TODO: Include packages needed for this application
-
-// CREATE readme file
-fs.open('README.md', 'w', function (err, file) {
-  if(err) throw err;
-  console.log('Saved!');
-});
+      // TODO: Include 
+      // fs.open('README.md', 'w', function (err, file) {
+      //   if(err) throw err;
+      //   console.log('Saved!');
+      // });
 
 
 // TODO: Create a function to write README file
